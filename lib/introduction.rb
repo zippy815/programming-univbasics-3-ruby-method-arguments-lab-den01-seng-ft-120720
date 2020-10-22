41772f2af4719625bd95cf8355fea5e3f0a5549b
Zipporah Horch <zipporahhorch@gmail.com>
2020-10-22 21:30:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,7 @@ introduction_with_language("Dan", "Ember.js")
 def introduction_with_language_optional(name="Edwin", language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
+introduction_with_language_optional
 
 
 
